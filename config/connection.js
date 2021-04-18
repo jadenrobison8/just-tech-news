f97249bec3aa543c25ece2515cac99741f172374
robisonja <jadenrobison8@gmail.com>
2021-04-18 17:52:53 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,24 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //create the connection to our databases, pass in your MYSQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'F67h3Yp4578jK3', {
+//change to configure to heroku
+//const sequelize = new Sequelize('just_tech_news_db', 'root', 'F67h3Yp4578jK3', {
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
+}
 
 module.exports = sequelize;
