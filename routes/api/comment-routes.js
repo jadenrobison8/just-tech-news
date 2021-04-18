95630a182282de58cd1228a8ba60fdd370001ae2
robisonja <jadenrobison8@gmail.com>
2021-04-18 17:45:27 -0600

    working comment model

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Comment, Post } = require('../../models');
+
+router.get('/', (req,res) => {
+    console.log('============');
+    Comment.findAll({
+        //query config
+        attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at' 
+        ],
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+router.post('/', (req,res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+      });
+});
+
+router.delete('/', (req,res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+      .then( dbCommentData => {
+          if (!dbCommentData) {
+              res.status(404).status.json({ message: 'No comment found with this id'});
+              return;
+          }
+          res.json(dbCommentData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+module.exports = router;
